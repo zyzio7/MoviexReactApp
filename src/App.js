d11649d331b0fe3913a7765fccb2ec5f3725e658
Michał Zyskowski <michal.zyskowski@sap.com>
2017-04-02 22:45:30 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+var Slider = require('react-slick');
+import logo from './logo.svg';
+import './App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+            <SimpleSlider/>
+        </p>
+      </div>
+    );
+  }
+}
+
+var SimpleSlider = React.createClass({
+    render: function () {
+        var settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 1,
+            slidesToScroll: 1
+        };
+        return (
+            <Slider {...settings}>
+                <div><h3>1</h3></div>
+                <div><h3>2</h3></div>
+                <div><h3>3</h3></div>
+                <div><h3>4</h3></div>
+                <div><h3>5</h3></div>
+                <div><h3>6</h3></div>
+            </Slider>
+        );
+    }
+});
+
+export default App;
